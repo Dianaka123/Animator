887c96086f305bd83f5a71d38a4ee0aef074f883
Di <diana.aristova.99@gmail.com>
2020-10-27 13:09:05 +0300

    Add animation

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    private Animator _animator;
+
+    private int Trigger = Animator.StringToHash( "New Trigger");
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Animator>() == null)
+        {
+            return;
+        }
+
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(Trigger );
+           
+        }
+    }
+}
---
+++ Assets/Scripts/RobotBoyController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RobotBoyController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+   
+    private int _run = Animator.StringToHash("IsRun");
+    private int _crunching = Animator.StringToHash("IsCrouch");
+    private int _jump = Animator.StringToHash("JumpTrigger");
+
+    private bool _isRun;
+    private bool _isCrunching;
+    
+    private void Reset()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            _isRun = true;
+            _animator.SetBool(_run,_isRun);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            _isRun = false;
+            _animator.SetBool(_run, _isRun);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            _isCrunching = true;
+            _animator.SetBool(_crunching, _isCrunching);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+        {
+            _isCrunching = false;
+            _animator.SetBool(_crunching, _isCrunching);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(_jump);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _animator.ResetTrigger(_jump);
+        }
+    }
+}

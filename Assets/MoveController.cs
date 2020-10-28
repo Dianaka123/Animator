166c2dee7e89e68e27d11931a2bb34b642d043c0
Di <diana.aristova.99@gmail.com>
2020-10-28 23:54:39 +0300

    Task 4
    Scene cudeStateBehaviour.

---
+++ Assets/MoveController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MoveController : MonoBehaviour
+{
+    private Animator animator;
+    private int moveTrigger = Animator.StringToHash("Move");
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool(moveTrigger, true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool(moveTrigger, false);
+        }
+    }
+}
---
+++ Assets/MoveStateBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MoveStateBehaviour : StateMachineBehaviour
+{
+    private readonly int colorTrigger = Animator.StringToHash("Color");
+    private float time = 0.0f;
+   
+    [SerializeField] private float duration = 1f; 
+
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Enter");
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (time >= 1.0f)
+        {
+            animator.SetBool(colorTrigger, true);
+            time %= 1;
+        }
+        Debug.Log(time);
+        time += Time.deltaTime;
+        //animator.SetBool(colorTrigger, false);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(colorTrigger, false);
+        Debug.Log("Exit");
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
